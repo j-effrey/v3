cf5f699dc4372dec552e00f94144cc41e7bbd6e4
Jeffrey Wong <jhw263@cornell.edu>
2020-08-04 19:48:48 -0400

    Updates

---
+++ precache-manifest.7556fdc5a8c69ab8ef2e4a3199811cfe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ca6ef45345a49f049762a1bf8964ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44581a147debd55fa26a",
+    "url": "/static/css/main.f0fa46d6.chunk.css"
+  },
+  {
+    "revision": "a0beeeb76d6e4eb4f1bd",
+    "url": "/static/js/2.efd5e4f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.efd5e4f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44581a147debd55fa26a",
+    "url": "/static/js/main.28514e7d.chunk.js"
+  },
+  {
+    "revision": "20feb87f852fab8380b0",
+    "url": "/static/js/runtime-main.30c0368b.js"
+  },
+  {
+    "revision": "9e94decf013d3e2c9adcc0b97cc5ce44",
+    "url": "/static/media/DroidSans.9e94decf.ttf"
+  },
+  {
+    "revision": "78c0de8abf66567262ee5e4e653fc11c",
+    "url": "/static/media/DroidSansMono.78c0de8a.ttf"
+  },
+  {
+    "revision": "24fb485463da083cde967169ed917304",
+    "url": "/static/media/Resume-2020.24fb4854.pdf"
+  },
+  {
+    "revision": "69d72522a3d5410b4854ee0086c008ad",
+    "url": "/static/media/squareme-min.69d72522.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f6f1a0330e46bcb4be02f2363ba666b.js"
+  "/precache-manifest.7556fdc5a8c69ab8ef2e4a3199811cfe.js"
 );
 
 self.addEventListener('message', (event) => {
